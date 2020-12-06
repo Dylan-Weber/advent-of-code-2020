501d9677c502f1695af1df83dba11f748e975d3f
Dylan Weber <34782089+Dylan-Weber@users.noreply.github.com>
2020-12-05 22:06:30 -0800

    Finished day 6

---
+++ day6/day6.py
@@ -0,0 +1,33 @@
+def part1(inp):
+    count = 0
+    curr = set()
+    for line in inp:
+        if line == '\n':
+            count += len(curr)
+            curr = set()
+        curr |= set(line[:-1])
+    count += len(curr)
+    return count
+
+
+def part2(inp):
+    count = 0
+    curr = None
+    for line in inp:
+        if line == '\n':
+            count += len(curr)
+            curr = None
+        elif curr is None:
+            curr = set(line[:-1])
+        else:
+            curr = curr.intersection(set(line[:-1]))
+    count += len(curr)
+    return count
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    with open('input.txt') as input_file:
+        inp = input_file.readlines()
+        print(f'Part 1: {part1(inp)}')
+        print(f'Part 2: {part2(inp)}')
